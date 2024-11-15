494fa34ccb923bb18c292559e2691fd2033ce3ad
SuzyKenne <suzyndatewo@gmail.com>
2024-11-15 01:09:58 +0100

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import Feedbacks from './components/Feedbacks';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <nav className="`${styles.paddingX}`"></nav>;
+};
+
+export default Navbar;
